27695231fd6d200677d911a1f7362817d0523d51
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-01-27 23:25:54 +0530

    Home Page Done

--- src/components/home/comeSoon/ComeSoon.jsx
+++ src/components/home/comeSoon/ComeSoon.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./comeSoon.css"
 
-import redDot from "../../../assets/redDot.png";
 
 class ComeSoon extends Component {
     render() {
@@ -36,8 +35,8 @@ class ComeSoon extends Component {
                     <p class="text-secondary">Hey, with Daksh around the corner, don't waste time looking up for the site. Type in your email and we'll keep you posted. </p>
                     <br />
                     <div class="container subscribe-form">
-                        <div class="col-9"><input type="email" className="subscribe" placeholder="Your email" size="35" /></div>
-                        <div class="col-3"><button type="submit" className="subscribe-btn">Subscribe</button></div>
+                        <input type="email" className="subscribe" placeholder="Your email" size="35" />
+                        <button type="submit" className="subscribe-btn">Subscribe</button>
                     </div>
                     <br /> <br /><br />
                 </div>
