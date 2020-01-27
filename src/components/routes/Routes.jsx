f9888aa3736a5527bcdc62324bfa4d6b6332b8fc
Vashanth <2000.vashanth@gmail.com>
2020-01-27 13:17:15 +0530

    Hackathon Registration

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -3,12 +3,14 @@ import {Switch,Route} from 'react-router-dom';
 
 import Home from '../home/Home';
 import Hackathon from '../hackathons/Hackathon';
+import Idea from '../hackathons/Idea'
 
 export default function Routes(){
     return(
         <Switch>
             <Route exact path='/' component= {Home}/>
             <Route exact path='/hackathon' component= {Hackathon}/>
+            <Route exact path='/idea' component = {Idea} />
         </Switch>
     )
 }
