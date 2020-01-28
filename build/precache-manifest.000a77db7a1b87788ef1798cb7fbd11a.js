a2dc142e4a86421e8b18ca6f3f2c8147b7516097
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-01-28 08:53:31 +0530

    Build-1

---
+++ build/precache-manifest.000a77db7a1b87788ef1798cb7fbd11a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec15cd3c97bc2a86dd1dde983e482a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f6e8ffe6a24be3bda5",
+    "url": "/static/css/2.feca2f6b.chunk.css"
+  },
+  {
+    "revision": "387c0eac3e5bd993bd39",
+    "url": "/static/css/main.a560ff18.chunk.css"
+  },
+  {
+    "revision": "22f6e8ffe6a24be3bda5",
+    "url": "/static/js/2.55b0b1fd.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.55b0b1fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "387c0eac3e5bd993bd39",
+    "url": "/static/js/main.2422383b.chunk.js"
+  },
+  {
+    "revision": "6ae1423a60cce249ba42",
+    "url": "/static/js/runtime-main.0d85306a.js"
+  },
+  {
+    "revision": "ae9d7d0debde1f1bbed54aea11de590f",
+    "url": "/static/media/Doodle.ae9d7d0d.png"
+  },
+  {
+    "revision": "305d146c1baf2fada8a39fb234f530dd",
+    "url": "/static/media/Ripple.305d146c.png"
+  },
+  {
+    "revision": "9f4b4297033d80a1b964b2ce91ff134f",
+    "url": "/static/media/UpperImage.9f4b4297.png"
+  },
+  {
+    "revision": "e700485e71f6ecbc389d96014ed509b5",
+    "url": "/static/media/arrow.e700485e.svg"
+  },
+  {
+    "revision": "24d5566d530bd8be574cf8a5aa6550a2",
+    "url": "/static/media/buildThings.24d5566d.svg"
+  },
+  {
+    "revision": "919d5322121d2655180c56f9301db1aa",
+    "url": "/static/media/calendar.919d5322.svg"
+  },
+  {
+    "revision": "6310857d6b720d04b7ab1188a99b1955",
+    "url": "/static/media/circles.6310857d.png"
+  },
+  {
+    "revision": "808acaf67b60b4a9384bae6053d1ad7c",
+    "url": "/static/media/concentricCircles.808acaf6.svg"
+  },
+  {
+    "revision": "4fe3acbcab887adcfad4818d3b53ba04",
+    "url": "/static/media/daksh.4fe3acbc.svg"
+  },
+  {
+    "revision": "86e1855d8d3ad81da051f316d8e66074",
+    "url": "/static/media/dakshLogo.86e1855d.svg"
+  },
+  {
+    "revision": "1f6c1e35237bca6408fd4e9a5145fffc",
+    "url": "/static/media/dakshWhite.1f6c1e35.png"
+  },
+  {
+    "revision": "fef207ac7e56cf481b37aed9318ee73a",
+    "url": "/static/media/dates.fef207ac.svg"
+  },
+  {
+    "revision": "374eaae6f6a3974d85e4018296755b95",
+    "url": "/static/media/developers.374eaae6.svg"
+  },
+  {
+    "revision": "5a465c2a1106ece5f38ed06aab799d93",
+    "url": "/static/media/donutStick.5a465c2a.svg"
+  },
+  {
+    "revision": "7e90ae3756156f88deed9eb2e47ba108",
+    "url": "/static/media/dsc.7e90ae37.svg"
+  },
+  {
+    "revision": "7130a50dc824c70f182e1370b0455765",
+    "url": "/static/media/fb.7130a50d.svg"
+  },
+  {
+    "revision": "6607cf52bebeb33c32d0f8f2854c49cb",
+    "url": "/static/media/hackathon.6607cf52.png"
+  },
+  {
+    "revision": "3a99397e8248cdc99dbc7ecc38e1ba91",
+    "url": "/static/media/insta.3a99397e.svg"
+  },
+  {
+    "revision": "0d422e755af864de714947a0bdfaaa46",
+    "url": "/static/media/paypal.0d422e75.png"
+  },
+  {
+    "revision": "7f33ca8fb0a1dd3f4dfa4e3323188eb1",
+    "url": "/static/media/pin.7f33ca8f.svg"
+  },
+  {
+    "revision": "721af8f6d4b1176e2f7f21433dbc36b2",
+    "url": "/static/media/sc.721af8f6.png"
+  },
+  {
+    "revision": "e4d140e125b0212b27ccc2a29f120537",
+    "url": "/static/media/tcs50.e4d140e1.png"
+  },
+  {
+    "revision": "fc59011f51c9de192e173a8b58ae0743",
+    "url": "/static/media/timeline.fc59011f.png"
+  },
+  {
+    "revision": "008623a2c95042160d61a30073bd079c",
+    "url": "/static/media/tn.008623a2.png"
+  },
+  {
+    "revision": "2c7e3d22e63577bf9f1c37d8444c85e6",
+    "url": "/static/media/tvs.2c7e3d22.png"
+  }
+]);
