572c14b1c99a2081b5989595469e7ccb0ceb1534
ayushsingh12march <ayushsingh12march@gmail.com>
2020-01-29 21:47:53 +0530

    made coming soon red dots and input form responsive

--- src/components/home/comeSoon/ComeSoon.jsx
+++ src/components/home/comeSoon/ComeSoon.jsx
@@ -7,21 +7,21 @@ class ComeSoon extends Component {
         return (
             <div>
                 <div class="container">
-                    <div class="cs">
+                    <div class="cs" >
                         <div>
-                            <div class="cs-content">
+                            <div class="cs-content"  >
                                 <div className="red-dot"></div>
                                 <p class="clusters">Workshops</p>
                             </div>
                         </div>
                         <div>
-                            <div class="cs-content">
+                            <div class="cs-content" >
                                 <div className="red-dot"></div>
                                 <p class="clusters">Powertalks</p>
                             </div>
                         </div>
                         <div>
-                            <div class="cs-content">
+                            <div class="cs-content" >
                                 <div className="red-dot"></div>
                                 <p class="clusters">Shows</p>
                             </div>
@@ -34,9 +34,9 @@ class ComeSoon extends Component {
                     <br />
                     <p class="text-secondary">Hey, with Daksh around the corner, don't waste time looking up for the site. Type in your email and we'll keep you posted. </p>
                     <br />
-                    <div class="container subscribe-form">
-                        <input type="email" className="subscribe" placeholder="Your email" size="35" />
-                        <button type="submit" className="subscribe-btn">Subscribe</button>
+                    <div class="container subscribe-form" >
+                        <input type="email" className="subscribe" placeholder="Your email" id="inputB1" />
+                        <button type="submit" className="subscribe-btn" id="inputB2">Subscribe</button>
                     </div>
                     <br /> <br /><br />
                 </div>
